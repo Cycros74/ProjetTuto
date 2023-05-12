fc12ef320088a356df1356f987d64789600f0a65
Cycros74 <ccros74@hotmail.fr>
2023-05-12 09:40:54 +0200

    ProjetTuto jusqu'à creation du formulaire d'inscription

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,21 +1,19 @@
 <?php
 
-namespace App;
+namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class HomeController extends AbstractController
-
 {
-    #[Route('/', 'home.index', methods: ['GET'])]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig', []);
 
     }
 
 }
-
-
